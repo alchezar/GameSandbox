9e03966001e4e1611b6897214f4b437579769ada
alchezar <kinder.wit@gmail.com>
2023-04-11 17:00:26 +0300

    Hud refactoring

--- Source/GameSandbox/P2/UI/STU_PlayerHUDWidget.cpp
+++ Source/GameSandbox/P2/UI/STU_PlayerHUDWidget.cpp
@@ -4,42 +4,39 @@
 #include "GameSandbox/P2/Component/STU_HealthComponent.h"
 #include "GameSandbox/P2/Component/STU_WeaponComponent.h"
 
-
 float USTU_PlayerHUDWidget::GetHealthPercent() const
 {
-	return GetOwnerComponent<USTU_HealthComponent>()->GetHealthPercent();
+	const USTU_HealthComponent* HealthComponent = GetOwnerComponent<USTU_HealthComponent>();
+	return HealthComponent ? HealthComponent->GetHealthPercent() : 0.f;
 }
 
 bool USTU_PlayerHUDWidget::GetCurrentWeaponUIData(FWeaponUIData& UIData) const
 {
-	return GetOwnerComponent<USTU_WeaponComponent>()->GetWeaponUIData(UIData);
+	const USTU_WeaponComponent* WeaponComponent = GetOwnerComponent<USTU_WeaponComponent>();
+	return WeaponComponent ? WeaponComponent->GetWeaponUIData(UIData) : false;
 }
 
 bool USTU_PlayerHUDWidget::GetCurrentAmmoData(FAmmoData& AmmoData) const
 {
-	return GetOwnerComponent<USTU_WeaponComponent>()->GetWeaponAmmoData(AmmoData);
+	const USTU_WeaponComponent* WeaponComponent = GetOwnerComponent<USTU_WeaponComponent>();
+	return WeaponComponent ? WeaponComponent->GetWeaponAmmoData(AmmoData) : false;
 }
 
 bool USTU_PlayerHUDWidget::IsPlayerAlive() const
 {
-	const auto HealthComponent = GetOwnerComponent<USTU_HealthComponent>();
+	const USTU_HealthComponent* HealthComponent = GetOwnerComponent<USTU_HealthComponent>();
 	return HealthComponent && !HealthComponent->IsDead();
 }
 
 bool USTU_PlayerHUDWidget::IsPlayerSpectating() const
 {
-	const auto Controller = GetOwningPlayer();
-	return Controller && Controller->GetStateName() == NAME_Spectating; 
+	const APlayerController* Controller = GetOwningPlayer();
+	return Controller && Controller->GetStateName() == NAME_Spectating;
 }
 
-template<class T>
+template <class T>
 T* USTU_PlayerHUDWidget::GetOwnerComponent() const
 {
-	const auto Player = GetOwningPlayerPawn();
-	if (!Player) return nullptr;
-
-	T* Component = Cast<T>(Player->GetComponentByClass(T::StaticClass()));
-	if (!Component) return nullptr;
-
-	return Component;
+	const APawn* Player = GetOwningPlayerPawn();
+	return Player ? Cast<T>(Player->GetComponentByClass(T::StaticClass())) : nullptr;
 }
